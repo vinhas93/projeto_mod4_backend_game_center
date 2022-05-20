d457e6521e7523f6f2bbfa1c5cf7e0382e5a6184
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-19 22:49:59 -0300

    chore: add swagger and class-validators

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Game Center')
+    .setDescription('Aplicação para gestão dos Jogos de cada usuário.')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('user')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
