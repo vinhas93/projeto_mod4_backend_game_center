76f9abcb156fe6d7ec75d194e652f3eb36c2435c
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-28 16:01:45 -0300

    feat(game): implemented entity

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,1 +1,12 @@
-export class Game {}
+export class Game {
+  id?: string;
+  title: string;
+  coverImageUrl: string;
+  description: string;
+  year: number;
+  imdbScore: number;
+  trailerYouTubeUrl: string;
+  gameplayYouTubeUrl: string;
+  gameGenders?: string[];
+  gameProfiles?: string[];
+}
