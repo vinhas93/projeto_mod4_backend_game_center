4f43ac1cf74352e6bb6e2281f4fbebe4c2fdbaa4
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-28 16:01:18 -0300

    feat(game): implemented service

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Game } from './entities/game.entity';
 
 @Injectable()
 export class GameService {
-  create(dto: CreateGameDto) {
-    return 'This action adds a new game';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateGameDto): Promise<Game> {
+    const data: Game = { ...dto };
+
+    return this.prisma.game.create({ data }).catch(this.handleError);
   }
 
-  findAll() {
-    return `This action returns all game`;
+  findAll(): Promise<Game[]> {
+    return this.prisma.game.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} game`;
+  async findOne(id: string) {
+    const record = await this.prisma.game.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o Id '${id}' não encontrado. `);
+    }
+
+    return record;
   }
 
-  update(id: number, dto: UpdateGameDto) {
-    return `This action updates a #${id} game`;
+  async update(id: string, dto: UpdateGameDto): Promise<Game> {
+    await this.findById(id);
+
+    const data: Partial<Game> = { ...dto };
+
+    try {
+      return this.prisma.game.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      return this.handleError(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} game`;
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.game.delete({
+      where: { id },
+    });
+  }
+
+  async findById(id: string): Promise<Game> {
+    const record = await this.prisma.game.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o Id '${id}' não encontrado. `);
+    }
+
+    return record;
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('/n');
+    const lastErrorLine = errorLines[errorLines.length - 1].trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação.',
+    );
   }
 }
