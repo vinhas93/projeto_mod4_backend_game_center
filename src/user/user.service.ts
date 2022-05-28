f15d1df9d5794b3d7fdca3561cb8463580170162
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-28 20:00:16 -0300

    feat(user): remove 'password' from service serialization

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,6 +12,17 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    name: true,
+    email: true,
+    password: false,
+    cpf: true,
+    isAdmin: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+
   constructor(private readonly prisma: PrismaService) {}
 
   async create(dto: CreateUserDto): Promise<User> {
@@ -26,11 +37,15 @@ export class UserService {
       password: await bcrypt.hash(dto.password, 10),
     };
 
-    return this.prisma.user.create({ data }).catch(this.handleError);
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(this.handleError);
   }
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: this.userSelect,
+    });
   }
 
   async findOne(id: string): Promise<User> {
@@ -64,6 +79,7 @@ export class UserService {
       return this.prisma.user.update({
         where: { id },
         data,
+        select: this.userSelect,
       });
     } catch (error) {
       return this.handleError(error);
@@ -79,7 +95,10 @@ export class UserService {
   }
 
   async findById(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } });
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if (!record) {
       throw new NotFoundException(`Registro com o Id '${id}' não encontrado. `);
